4d945a4da45c2d411809bb594909c25eb9d83e9c
AndreyR <rubanoff.and@yandex.by>
2025-12-01 10:47:26 +0300

    Create keys.sh

---
+++ keys.sh
@@ -0,0 +1,318 @@
+# ============================================
+# 80+ ТИПОВ СЕКРЕТОВ ДЛЯ ТЕСТИРОВАНИЯ TRUFFLEHOG
+# ============================================
+
+# ----- 1. AWS -----
+AKIAIOSFODNN7EXAMPLE
+aws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+
+# ----- 2. GitHub -----
+ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+github_pat_11ABC_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 3. GitLab -----
+glpat-xxxxxxxxxxxxxxxxxxxx
+glptt-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 4. Bitbucket -----
+bitbucket_app_password=xxxxxxxxxxxxxxxxxxxx
+
+# ----- 5. Stripe -----
+sk_live_4eC39HqLyjWDarjtT1zdp7dc
+rk_live_xxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 6. Stripe Webhook -----
+whsec_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 7. PayPal -----
+access_token$production$xxxxxxxxxxxxx$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 8. Square -----
+sq0atp-xxxxxxxxxxxxxxxxxxxxxx
+EAAAE_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 9. Slack Bot -----
+xoxb-123456789012-1234567890123-AbCdEfGhIjKlMnOpQrStUv
+
+# ----- 10. Slack User -----
+xoxp-123456789012-123456789012-1234567890123-abcdef01234567890
+
+# ----- 11. Slack Webhook -----
+https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
+
+# ----- 12. Discord Webhook -----
+https://discord.com/api/webhooks/123456789012345678/AbCdEfGhIjKlMnOpQrStUvWx
+
+# ----- 13. Discord Bot -----
+NTxxxxxxxxxxxxxxxxxx.Xxxxxx.Xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 14. Telegram Bot -----
+1234567890:AAHxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 15. Twilio Account SID -----
+AC1234567890abcdef1234567890abcdef
+
+# ----- 16. Twilio Auth Token -----
+twilio_auth_token=a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6
+
+# ----- 17. Twilio API Key -----
+SKxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 18. SendGrid -----
+SG.xxxxxxxxxxxxxxxxxxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 19. Mailgun -----
+key-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 20. Mailchimp -----
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-us01
+
+# ----- 21. Postmark -----
+xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+# ----- 22. Google API Key -----
+AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe
+
+# ----- 23. Google OAuth -----
+GOCSPX-xxxxxxxxxxxxxxxxxxxxxxxx
+ya29.a0AfH6SMBxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 24. Firebase -----
+AIzaSyxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+firebase_api_key=AIzaSyxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 25. GCP Service Account -----
+"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBg..."
+
+# ----- 26. Azure Storage -----
+DefaultEndpointsProtocol=https;AccountName=test;AccountKey=xxxxbase64xxxx==
+
+# ----- 27. Azure Client Secret -----
+azure_client_secret=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+# ----- 28. Azure SAS Token -----
+?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacup&se=2024-01-01&sig=xxxxx
+
+# ----- 29. Heroku -----
+HEROKU_API_KEY=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+# ----- 30. DigitalOcean -----
+dop_v1_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 31. Linode -----
+linode_api_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 32. Vultr -----
+vultr_api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 33. Cloudflare API -----
+cloudflare_api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 34. Cloudflare Token -----
+CF_API_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 35. Vercel -----
+vercel_token=xxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 36. Netlify -----
+netlify_access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 37. NPM -----
+npm_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 38. PyPI -----
+pypi-AgEIcHlwaS5vcmcxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 39. NuGet -----
+oy2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 40. Docker Hub -----
+dckr_pat_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 41. Datadog -----
+ddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 42. New Relic -----
+NRAK-XXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+# ----- 43. Sentry -----
+sentry_dsn=https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx@sentry.io/123456
+
+# ----- 44. PagerDuty -----
+pagerduty_api_key=u+xxxxxxxxxxxxxxxxxx
+
+# ----- 45. Splunk -----
+splunk_token=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+# ----- 46. Sumo Logic -----
+sumo_access_id=xxxxxxxxxxxx
+sumo_access_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 47. Elastic Cloud -----
+elastic_cloud_api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 48. MongoDB Atlas -----
+mongodb+srv://user:password123@cluster0.xxxxx.mongodb.net
+
+# ----- 49. Redis Labs -----
+redis://default:xxxxxxxxxxxxxxxxxxxx@redis-12345.c1.us-east-1.ec2.cloud.redislabs.com:12345
+
+# ----- 50. Postgres -----
+postgres://admin:SuperSecret123@db.example.com:5432/production
+
+# ----- 51. MySQL -----
+mysql://root:MyP@ssw0rd@mysql.example.com:3306/database
+
+# ----- 52. Auth0 -----
+auth0_client_secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 53. Okta -----
+okta_api_token=00xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 54. OneLogin -----
+onelogin_client_secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 55. Algolia -----
+algolia_admin_api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 56. Elasticsearch -----
+elasticsearch_password=xxxxxxxxxxxxxxxxxxxx
+
+# ----- 57. Shopify -----
+shppa_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+shpss_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 58. WooCommerce -----
+wc_consumer_secret=cs_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 59. Zendesk -----
+zendesk_api_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 60. Intercom -----
+intercom_access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 61. HubSpot -----
+hubspot_api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+# ----- 62. Salesforce -----
+salesforce_security_token=xxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 63. Airtable -----
+airtable_api_key=keyXXXXXXXXXXXXXXXX
+
+# ----- 64. Notion -----
+secret_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 65. Asana -----
+asana_personal_access_token=1/1234567890123456:xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 66. Jira -----
+jira_api_token=xxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 67. Confluence -----
+confluence_api_token=xxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 68. Trello -----
+trello_api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 69. Linear -----
+lin_api_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 70. CircleCI -----
+circle_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 71. Travis CI -----
+travis_api_token=xxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 72. Jenkins -----
+jenkins_api_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 73. Buildkite -----
+bkua_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 74. CodeClimate -----
+codeclimate_api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 75. Coveralls -----
+coveralls_repo_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 76. SonarQube -----
+sonar_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 77. Snyk -----
+SNYK_TOKEN=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+# ----- 78. OpenAI -----
+sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 79. Anthropic -----
+sk-ant-api03-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 80. Cohere -----
+cohere_api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 81. Hugging Face -----
+hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 82. Replicate -----
+r8_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 83. Mapbox -----
+pk.eyJ1Ijoixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 84. Google Maps -----
+google_maps_api_key=AIzaSyxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 85. HERE Maps -----
+here_api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 86. Plaid -----
+plaid_secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 87. Braintree -----
+braintree_private_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 88. Adyen -----
+adyen_api_key=AQExxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 89. RSA Private Key -----
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA04up8hoqzS1+APIB0RhjXyObwExample
+-----END RSA PRIVATE KEY-----
+
+# ----- 90. SSH Private Key -----
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQExample
+-----END OPENSSH PRIVATE KEY-----
+
+# ----- 91. PGP Private Key -----
+-----BEGIN PGP PRIVATE KEY BLOCK-----
+lQPGBGExamplePGPkey
+-----END PGP PRIVATE KEY BLOCK-----
+
+# ----- 92. JWT Token -----
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.dozjgNryP4J3jVmNHl0w5N
+
+# ----- 93. Basic Auth -----
+Authorization: Basic YWRtaW46cGFzc3dvcmQxMjM=
+
+# ----- 94. Bearer Token -----
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.xxx
+
+# ----- 95. Grafana -----
+grafana_api_key=eyJrIjoixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 96. Terraform Cloud -----
+terraform_cloud_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.atlasv1.xxxxxx
+
+# ----- 97. Vault -----
+vault_token=hvs.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 98. Pulumi -----
+pulumi_access_token=pul-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 99. Doppler -----
+dp.st.xxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# ----- 100. 1Password -----
+ops_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
