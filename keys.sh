2cb30b2c3405e33e0c8f85a6f214cf3ed7a47e63
AndreyR <rubanoff.and@yandex.by>
2025-12-01 11:01:46 +0300

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -1,149 +1,302 @@
-# ===== AWS (детектируется по паттерну AKIA + 16 символов) =====
+# ----- AWS -----
 AKIAIOSFODNN7EXAMPLE
-AKIAI44QH8DHBEXAMPLE
+ASIAZZZZZZZZZEXAMPLE
 aws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+aws_session_token=FwoGZXIvYXdzEBYaDKL5sampletoken
 
-# ===== GitHub Personal Access Token (ghp_ + 36 символов) =====
-ghp_wWPw5k4aXcaT4fNP0UcnZwJUVFk6LO2rINUx
-ghp_R2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+# ----- GitHub -----
+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
+gho_16C7e42F292c6912E7710c838347Ae178B4a
+ghs_cCOHfPYJkxnTgRytHCIcXrRuTslNpyRe1Zps
+ghr_1B4a2e77838347a7E420ce178F2E7c6912E169gC
+github_pat_11ABRF6LA0yXzPiALbLcD4_5TjvkRuQVSHzSsLmNpLwJkcV2ZGGHsqJ7vStRZ3Y3AIORTU4TXCtest
 
-# ===== GitHub Fine-grained PAT =====
-github_pat_11ABCDE2Y0000000000000_00000000000000000000000000000000000000000000000000
+# ----- GitLab -----
+glpat-ABCDEF1234567890abcd
+glptt-1234567890abcdef1234567890abcdef12345678
 
-# ===== Slack Bot Token (xoxb-) =====
-xoxb-123456789012-1234567890123-abcdefghijklmnopqrstuvwx
+# ----- Slack -----
+xoxb-123456789012-1234567890123-AbCdEfGhIjKlMnOpQrStUvWx
+xoxp-123456789012-123456789012-123456789012-abcdef1234567890abcdef1234567890
+xoxa-2-123456789012-123456789012-1234567890123-abcdef1234
+xoxr-123456789012-123456789012-1234567890123-abcdef1234ab
+https://hooks.slack.com/services/T01234ABC/B01234DEF/aBcDeFgHiJkLmNoPqRsTuVwX
 
-# ===== Slack Webhook =====
-https://hooks.slack.com/services/T01234567/B01234567/abcdefghijklmnopqrstuvwx
+# ----- Discord -----
+https://discord.com/api/webhooks/123456789012345678/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890AB
+MTIzNDU2Nzg5MDEyMzQ1Njc4OQ.Zamw9Q.abcdefghijklmnopqrstuvwxyz12
 
-# ===== Stripe Live Key =====
-sk_live_51H7xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-sk_test_51H7xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+# ----- Stripe -----
+sk_live_51HG7J9KZvKYlo2C0xNASDFGHJ1234567890abcdefghijklmnopqrstuvwxyz
+sk_test_51HG7J9KZvKYlo2C0xNASDFGHJ1234567890abcdefghijklmnopqrstuvwxyz
+rk_live_51HG7J9KZvKYlo2C0xNASDFGHJ1234567890abcdefghijklmnop
+pk_live_51HG7J9KZvKYlo2C0xNASDFGHJ1234567890abcdefghijklmnop
+whsec_abcdefghijklmnopqrstuvwxyz123456
 
-# ===== Google API Key (AIzaSy + 33 символа) =====
-AIzaSyABcdEfGhIjKlMnOpQrStUvWxYz1234567
+# ----- Google / GCP -----
+AIzaSyAa1b2C3d4E5f6G7h8I9j0K1L2M3N4O5P6
+ya29.a0AfH6SMBxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+GOCSPX-abcdefghijklmnopqrstuvwxyz12
 
-# ===== Twilio =====
-TWILIO_ACCOUNT_SID=AC12345678901234567890123456789012
-TWILIO_AUTH_TOKEN=a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6
+# ----- Azure -----
+DefaultEndpointsProtocol=https;AccountName=mystorageaccount;AccountKey=abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcd==;EndpointSuffix=core.windows.net
+eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImtnMkxZczJUMENUaklmajRydDZKSXluZW4zOCIsImtpZCI6ImtnMkxZczJUMENUaklmajRydDZKSXluZW4zOCJ9
 
-# ===== SendGrid (SG. + длинный ключ) =====
-SG.abcdefghijklmnop.qrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUV
+# ----- Twilio -----
+AC1234567890abcdef1234567890abcd12
+SK1234567890abcdef1234567890abcd12
+twilio_auth_token=a1234567890bcdef1234567890abcdef
 
-# ===== Mailgun =====
+# ----- SendGrid -----
+SG.abcdefghijklmnopqrst.uvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ012345
+
+# ----- Mailgun -----
 key-1234567890abcdef1234567890abcdef
+pubkey-1234567890abcdef1234567890abcdef
 
-# ===== Mailchimp (32 символа-dc) =====
-a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6-us21
+# ----- Mailchimp -----
+1234567890abcdef1234567890abcdef-us21
 
-# ===== Heroku API Key (UUID формат) =====
+# ----- Telegram -----
+1234567890:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsawU
+
+# ----- OpenAI -----
+sk-proj-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTU1234567890abc
+sk-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNO12345678
+
+# ----- Anthropic -----
+sk-ant-api03-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGH
+
+# ----- HuggingFace -----
+hf_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890
+
+# ----- Cohere -----
+abcdefghij1234567890ABCDEFGHIJ1234567890
+
+# ----- Replicate -----
+r8_abcdefghijklmnopqrstuvwxyz1234567890AB
+
+# ----- Heroku -----
 HEROKU_API_KEY=12345678-abcd-1234-abcd-1234567890ab
 
-# ===== DigitalOcean =====
+# ----- DigitalOcean -----
 dop_v1_1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef
+doo_v1_1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef
 
-# ===== NPM Token =====
-npm_1234567890abcdefghijABCDEFGHIJ123456
+# ----- Linode -----
+linode_api_token=1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef
 
-# ===== Discord Webhook =====
-https://discord.com/api/webhooks/123456789012345678/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789012345
+# ----- Vultr -----
+vultr_api_key=ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234
 
-# ===== Discord Bot Token =====
-MTIzNDU2Nzg5MDEyMzQ1Njc4.AbCdEf.abcdefghijklmnopqrstuvwxyz1234
+# ----- Cloudflare -----
+CF_API_KEY=1234567890abcdef1234567890abcdef12345
+CF_API_TOKEN=abcdefghijklmnopqrstuvwxyz_1234567890AB
 
-# ===== Telegram Bot Token =====
-1234567890:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw
+# ----- Vercel -----
+vercel_token=abcdefghijklmnopqrstuvwx
 
-# ===== OpenAI =====
-sk-proj-abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGH
+# ----- Netlify -----
+netlify_access_token=abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGH
 
-# ===== Private RSA Key (полный формат) =====
------BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEA0Z3VS5JJcds3xfn/ygWyF8PbnGy0AHB7MhtwIQC7jeyV6rK7
-n0bMxwLRvXYofL/h2sSa0p4AZVZ6TQkByPMsXpHy1dPfGtqiB8VT8dg5L+azT1D5
-wAEkPhoxkhCnxq/IamJplOXCf7DJL0dwXP9KxfJPHsoNoZYdBs64xEA7bFJLFJBa
-5rXV8j/IjszLd0TMxQE5B1dVcjfp+Fy8AeSe8xKpqjGzMb/j3sQMmcJDE0qKP9Xd
-Gj8Cnxrb8VJD6h5TH7gDquil6K7DDLsq3p0J/IVggCNDp1DYJQ7kIzD8sFeFpGCk
-5qLAIaVJJNARIZ8pJwIDAQABAoIBAE+DgPrCc7KP1PFGUA/8D8NLnw6v9M4EvTgR
------END RSA PRIVATE KEY-----
+# ----- NPM -----
+npm_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890
 
-# ===== Private SSH Key =====
------BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAlwAAAAdzc2gtcn
-NhAAAAAwEAAQAAAIEA0Z3VS5JJcds3xfn/ygWyF8PbnGy0AHB7MhtwIQC7jeyV6rK7n0bM
------END OPENSSH PRIVATE KEY-----
+# ----- PyPI -----
+pypi-AgEIcHlwaS5vcmcCJDU2NzhlY2Y2LWY0ZjAtNGYyNy04YTJhLWY4ODY3MjY4Mjg0NgACKlszLCJmNGU5Zj
 
-# ===== Database Connection Strings =====
-postgres://admin:password123@prod-db.example.com:5432/myapp
-mysql://root:SuperSecretP@ss@mysql.internal:3306/production
-mongodb://dbuser:MongoPass123@cluster0.abc123.mongodb.net:27017/app
-mongodb+srv://admin:SecurePassword@cluster.xxxxx.mongodb.net/database
+# ----- Docker Hub -----
+dckr_pat_aBcDeFgHiJkLmNoPqRsTuVwXyZ
 
-# ===== JWT (валидный Base64 формат) =====
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ
-
-# ===== Azure =====
-DefaultEndpointsProtocol=https;AccountName=mystorageaccount;AccountKey=4dVw+l0WuGxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx==;EndpointSuffix=core.windows.net
-
-# ===== Datadog =====
+# ----- Datadog -----
+ddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 DD_API_KEY=1234567890abcdef1234567890abcdef
+DD_APP_KEY=1234567890abcdef1234567890abcdef12345678
 
-# ===== New Relic =====
+# ----- New Relic -----
 NRAK-ABCDEF1234567890ABCDEF1234
+NRII-abcdefghijklmnopqrstuvwxyz123456
 
-# ===== Sentry DSN =====
+# ----- Sentry -----
 https://1234567890abcdef1234567890abcdef@o123456.ingest.sentry.io/1234567
 
-# ===== Shopify =====
+# ----- PagerDuty -----
+u+abcdefghijklmnopqrst
+
+# ----- Splunk -----
+Splunk 12345678-1234-1234-1234-123456789012
+
+# ----- Elastic -----
+elastic_api_key=abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMN
+
+# ----- Grafana -----
+eyJrIjoiMTIzNDU2Nzg5MGFiY2RlZiIsIm4iOiJhcGkta2V5IiwiaWQiOjF9
+glsa_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890abcdefghijklmno_12345678
+glc_eyJvIjoiMTIzNDU2IiwibiI6Im15LXRva2VuIiwiayI6ImFiY2RlZmdoaWoiLCJtIjp7InIiOiJ1cyJ9fQ==
+
+# ----- MongoDB Atlas -----
+mongodb+srv://admin:MySecureP4ssw0rd@cluster0.abcde.mongodb.net/mydb
+
+# ----- PostgreSQL -----
+postgres://admin:SuperSecretPass123@db.example.com:5432/production
+postgresql://user:p@ssw0rd@prod-db.internal:5432/app
+
+# ----- MySQL -----
+mysql://root:MySQLR00tP@ss!@mysql.example.com:3306/database
+
+# ----- Redis -----
+redis://default:RedisPassword123@redis-12345.ec2.cloud.redislabs.com:12345/0
+rediss://:authtoken@redis-server:6380
+
+# ----- Shopify -----
 shppa_1234567890abcdef1234567890abcdef
 shpss_1234567890abcdef1234567890abcdef
+shpat_1234567890abcdef1234567890abcdef
 
-# ===== Algolia =====
-ALGOLIA_API_KEY=a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6
+# ----- Square -----
+sq0atp-abcdefghijklmnopqrstuv
+EAAAE1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 
-# ===== HuggingFace =====
-hf_ABCDefghIJKLmnopQRSTuvwxYZ123456
+# ----- PayPal -----
+access_token$production$abcdefgh12345678$1234567890abcdef1234567890abcdef
 
-# ===== GitLab =====
-glpat-Ab1Cd2Ef3Gh4Ij5Kl6Mn
+# ----- Braintree -----
+BRAINTREE_ACCESS_TOKEN=access_token$production$abcdefgh$1234567890abcdef
 
-# ===== Cloudflare =====
-CF_API_KEY=1234567890abcdef1234567890abcdef12345
-
-# ===== Basic Auth Header =====
-Authorization: Basic YWRtaW46cGFzc3dvcmQxMjM0NTY=
-
-# ===== PGP Private Key =====
------BEGIN PGP PRIVATE KEY BLOCK-----
-lQOYBF0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345
------END PGP PRIVATE KEY BLOCK-----
-
-# ===== Docker Hub =====
-DOCKER_PASSWORD=dckr_pat_abcdefghijklmnopqrstuvwxyz
-
-# ===== CircleCI =====
-CIRCLE_TOKEN=1234567890abcdef1234567890abcdef12345678
-
-# ===== Terraform Cloud =====
-TFC_TOKEN=abcdefghijklmnop.atlasv1.1234567890abcdefghijklmnopqrstuvwxyz
-
-# ===== Vault Token =====
-VAULT_TOKEN=hvs.1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN
-
-# ===== Grafana =====
-GRAFANA_API_KEY=eyJrIjoiMTIzNDU2Nzg5MGFiY2RlZjEyMzQ1Njc4OTBhYmNkZWYiLCJuIjoiYXBpLWtleSIsImlkIjoxfQ==
-
-# ===== Firebase =====
-FIREBASE_API_KEY=AIzaSyA1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q
-
-# ===== Plaid =====
+# ----- Plaid -----
+PLAID_CLIENT_ID=1234567890abcdef12345678
 PLAID_SECRET=1234567890abcdef1234567890abcdef
 
-# ===== Braintree =====
-BRAINTREE_PRIVATE_KEY=1234567890abcdef1234567890abcdef
+# ----- Algolia -----
+ALGOLIA_ADMIN_KEY=a1234567890bcdef1234567890abcdef
 
-# ===== Auth0 =====
-AUTH0_CLIENT_SECRET=1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef
+# ----- Airtable -----
+keyAbCdEfGhIjKlMnO
+patabcdefgh12345.1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef
 
-# ===== Okta =====
-OKTA_API_TOKEN=00abcdefghijklmnopqrstuvwxyz1234567890AB
+# ----- Notion -----
+secret_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNO
+
+# ----- Asana -----
+1/1234567890123456:abcdefghijklmnopqrstuvwxyz123456
+
+# ----- Jira / Atlassian -----
+ATATT3xFfGF0abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnop
+
+# ----- Linear -----
+lin_api_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890ab
+
+# ----- Trello -----
+trello_api_key=1234567890abcdef1234567890abcdef
+
+# ----- Zendesk -----
+zendesk_api_token=abcdefghijklmnopqrstuvwxyz1234567890ABCD
+
+# ----- Intercom -----
+intercom_access_token=dG9rOmFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6MTIzNDU2Nzg5
+
+# ----- HubSpot -----
+pat-na1-12345678-abcd-1234-abcd-1234567890ab
+
+# ----- Salesforce -----
+00D000000000000!AQEAQAbcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUV
+
+# ----- Auth0 -----
+AUTH0_CLIENT_SECRET=abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZab
+
+# ----- Okta -----
+00abcdefghijklmnopqrstuvwxyz1234567890AB
+
+# -----LCJhIjoiY2xhYmNkZWZnMDEyMzQ1Njc4OTBhYmNkZWYifQ.abcdefghijklmnopqrstuvwxyz
+
+# ----- CircleCI -----
+CIRCLE_TOKEN=1234567890abcdef1234567890abcdef12345678
+
+# ----- Travis CI -----
+travis_api_token=aBcDeFgHiJkLmNoPqRsT
+
+# ----- Jenkins -----
+JENKINS_API_TOKEN=11abcdefghijklmnopqrstuvwxyz1234
+
+# ----- Buildkite -----
+bkua_1234567890abcdef1234567890abcdef12345678
+
+# ----- SonarQube -----
+squ_1234567890abcdef1234567890abcdef12345678
+
+# ----- Snyk -----
+SNYK_TOKEN=12345678-abcd-1234-abcd-1234567890ab
+
+# ----- Terraform Cloud -----
+abcdefghijklmnop.atlasv1.1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTU
+
+# ----- Vault -----
+hvs.CAESIAbcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOP
+hvb.AAAAAQabcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJ
+
+# ----- Pulumi -----
+pul-1234567890abcdef1234567890abcdef12345678
+
+# ----- Doppler -----
+dp.st.production.abcdefghijklmnopqrstuvwxyz1234567890ABCDEF
+
+# ----- 1Password -----
+ops_eyJzaWduSW5BZGRyZXNzIjoibXkuMXBhc3N3b3JkLmNvbSIsInVzZXJBdXRoIjp7Im1ldG
+
+# ----- Private Keys -----
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEA04up8hoqzS1+APIB0RhjXyObwHQnOzhAk5Bd7mhkSbPkyhP1
+1QEXAMPLEKEYMATERIALHERE0Z3VS5JJcds3xfn/ygWyF8PbnGy0AHB7MhtwIQC7
+jeyV6rK7n0bMxwLRvXYofL/h2sSa0p4AZVZ6TQkByPMsXpHy1dPfGtqiB8VT8dg5
+L+azT1D5wAEkPhoxkhCnxq/IamJplOXCf7DJL0dwXP9KxfJPHsoNoZYdBs64xEA7
+-----END RSA PRIVATE KEY-----
+
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAYEAxampleopensshprivatekeycontenthere1234567890abcdefgh
+ijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnop
+-----END OPENSSH PRIVATE KEY-----
+
+-----BEGIN EC PRIVATE KEY-----
+MHQCAQEEIFexampleecprivatekeycontent1234567890abcdefghijklmnopqrstuvwx
+-----END EC PRIVATE KEY-----
+
+-----BEGIN DSA PRIVATE KEY-----
+MIIBuwIBAAKBgQDexampledsakey0Z3VS5JJcds3xfn/ygWyF8PbnGy0AHB7Mht
+-----END DSA PRIVATE KEY-----
+
+-----BEGIN PGP PRIVATE KEY BLOCK-----
+lQPGBGVexamplepgpprivatekeycontenthere1234567890abcdefghijklmnopqrstuv
+-----END PGP PRIVATE KEY BLOCK-----
+
+# ----- JWT Tokens -----
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.tBtestjwt12345
+
+# ----- Basic Auth -----
+Authorization: Basic YWRtaW46U3VwZXJTZWNyZXRQYXNzd29yZDEyMzQ1Ng==
+Basic dXNlcm5hbWU6cGFzc3dvcmQxMjM0NTY3ODkw
+
+# ----- Bearer Tokens -----
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.test123
+
+# ----- Generic API Keys (с контекстом) -----
+API_KEY=abcdefghijklmnopqrstuvwxyz123456
+api_secret=1234567890abcdef1234567890abcdef
+PRIVATE_KEY=supersecretprivatekey1234567890ab
+access_token=abcdefghijklmnopqrstuvwxyz123456789012345678
+
+# ----- Facebook -----
+EAACEdEose0cBAabcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUV
+
+# ----- Twitter -----
+AAAAAAAAAAAAAAAAAAAAAabcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# ----- Instagram -----
+IGQVJYabcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop
+
+# ----- LinkedIn -----
+AQXdabcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# ----- Bitbucket -----
+bbdc_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890ABCDEFGHIJKLMNOPQ
